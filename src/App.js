eb3ef6f808638ef50d692bc66b3141267c73a442
Hồng Nhung <lenhung14101999@gmail.com>
2023-09-14 21:26:01 +0700

    react profile-card

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <div className='container'>
+        <div className='card'>
+            <div className='card--header' />
+            <img 
+            className='avatar'
+            src="https://scontent.fdad3-4.fna.fbcdn.net/v/t39.30808-6/330445283_220291300481988_2383491924458735024_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=1b51e3&_nc_ohc=4v4PYALCVB4AX_qSNoO&_nc_ht=scontent.fdad3-4.fna&oh=00_AfCa9WrivByTQdivSel5stFihcC4C-ldX57uS7zQqTFPHg&oe=65075395" 
+            alt="avatar" 
+            />
+            <div className='card--body'>
+                <div>
+                  <p className='text--header'>Phương Long</p>
+                  <p className='text--sub'>
+                  <i class="fab fa-instagram"></i>Intagram: __25thang4_
+                  </p>
+                  <button type='button' className='btn third' >
+                      <a href="https://www.instagram.com/__25thang4_/" target="_blank">FOLLOW ME</a>
+                  </button>
+                </div>
+            </div>
+        </div>
     </div>
-  );
-}
+  </> 
 
-export default App;
+
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
--- src/index.js
+++ src/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './App.css';
+
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div className="container">
+  <div className="card">
+    <div className="card--header" />
+    <img 
+          className='avatar'
+         
+          src="https://scontent.fdad1-2.fna.fbcdn.net/v/t39.30808-6/376882998_3636573219906724_5251353149889406288_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=5614bc&_nc_ohc=hSR-zx9csYsAX8lsk87&_nc_ht=scontent.fdad1-2.fna&oh=00_AfAppHCiBbGxZlmoty3DwE5IdKx5hbyyIlMCSyLd81AbTw&oe=65080901" 
+          alt="avatar" 
+          />
+    <div className="card--body">
+      <div>
+        <p className="text-header">Lê Thị Hồng Nhung</p>
+        <p className="text-sub">
+          14-10-1999
+        </p>
+        <button className="btn third">FOLLOW</button>
+      </div>
+    </div>
+  </div>
+</div>
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
